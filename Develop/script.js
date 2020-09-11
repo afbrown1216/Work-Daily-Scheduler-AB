a282a9dc84ed71549d76250b39a00ed114047c57
Phantom <fay.ashbro@gmail.com>
2020-09-11 00:16:42 -0400

    psedo code

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,37 @@
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
+// ajax get call to return the current day in the #currentDay element 
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
+// create blocks of time for the day with an ajax get call of the hours 9AM -5PM and save button that triggers the setlocalstorage
+// hours col
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// create a conditional statment to change the color styling of the block based on past present or future 
 // WHEN I click into a timeblock
 // THEN I can enter an event
+// when you click on the itmeblock make it an input or modal hmmmm 
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
+//do a get set for that time block probaly save the time and text 
 // WHEN I refresh the page
 // THEN the saved events persist
+//create a getlocalstorage at the top of the script to get it from time probaly a document ready
+
+
+
+
+//variables 
+//#currentDay 
+//#container for time blocks 
+
+//event listeners 
+// save button in timeblock to setthe local storage 
+
+
+//functions 
+//
+
+
+
