6223936bd26a6cb1c92b158ce1428a52c481f048
Phantom <fay.ashbro@gmail.com>
2020-09-12 17:39:31 -0400

    dymanically loaded timeblocks added

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,9 @@
 // GIVEN I am using a daily planner to create a schedule
 
 
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// create blocks of time for the day with an ajax get call of the hours 9AM -5PM and save button that triggers the setlocalstorage
+
 // hours col
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// create a conditional statment to change the color styling of the block based on past present or future 
+
 // WHEN I click into a timeblock
 // THEN I can enter an event
 // when you click on the itmeblock make it an input or modal hmmmm 
@@ -21,7 +17,7 @@
 
 
 
-getCurrentDay();
+
 
 
 
@@ -29,6 +25,7 @@ getCurrentDay();
 //#currentDay 
 //#container for time blocks 
 var currentDayEl = $("#currentDay");
+var containerEl = $(".container");
 
 //event listeners 
 // save button in timeblock to setthe local storage 
@@ -48,3 +45,72 @@ function getCurrentDay (){
 }
 
 
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// create blocks of time for the day with an ajax get call of the hours 9AM -5PM and save button that triggers the setlocalstorage
+
+function createTimeBlocks () {
+    //for loop that creates 8 columns with the time 
+    for (var i = 9; i > 1;i--){
+        //create a row 
+    var row = $("<div>");
+    console.log(row);
+    //add attributes to the div 
+    row.attr("class","row");
+    //create the columns 
+    //time column
+    var timeCol = $("<div>")
+    var time = $("<h3>");
+    timeCol.attr("class", "col-sm-2 hour");
+   
+    //textarea column 
+    var textCol = $("<textarea>");
+    textCol.attr("class", "col-sm-8 time-block");
+    textCol.text("Todo");
+    // figure out the time block coloring conditional statement 
+    //addClass() removeClass() 
+    //save button column 
+    var saveCol = $("<div>");
+    var saveImg = $("<img>")
+    saveCol.attr("class", "col-sm-2 saveBtn btn");
+    // saveImg.attr("src","https://cdn2.iconfinder.com/data/icons/actions-states-vol-1-colored/48/JD-03-512.png" );
+   saveCol.text("save");
+    
+
+    containerEl.append(row);
+    row.append(timeCol);
+    timeCol.append(time);
+    row.append(textCol);
+    row.append(saveCol);
+    // saveCol.append(saveImg);
+
+    }
+    
+
+
+
+}
+
+
+
+
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// create a conditional statment to change the color styling of the block based on past present or future 
+
+// function timeIndicator (){
+//     if()
+// }
+
+
+
+$(document).ready(function () {
+    getCurrentDay();
+    createTimeBlocks();
+
+    
+
+
+
+})
